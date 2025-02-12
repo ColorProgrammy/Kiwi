4180e7724d3de243a745078754dfb85381dfdfb5
yupi <aconmonuser@gmail.com>
2025-02-12 22:56:23 +0300

    Update interpreter.hpp

--- kiwi/interpreter.hpp
+++ kiwi/interpreter.hpp
@@ -14,52 +14,57 @@ private:
     std::vector<std::string> script_lines;
     std::map<std::string, size_t> function_locations;
     std::stack<size_t> call_stack;
+    std::stack<size_t> loop_stack;
+    std::stack<size_t> if_stack;
     size_t current_line = 0;
     bool exit_requested = false;
-    std::stack<size_t> loop_ends;
+    bool skip_mode = false;
+    int skip_depth = 0;
+    bool break_requested = false;
 
     int safe_stoi(const std::string& s) {
         if (s.empty()) throw std::invalid_argument("Empty string");
-        for (char c : s) {
-            if (!isdigit(c)) throw std::invalid_argument("Invalid number");
-        }
+        for (char c : s) if (!isdigit(c)) throw std::invalid_argument("Invalid number");
         return std::stoi(s);
-    }
-
-    std::string get_var_value(const std::string& var_name) {
-        auto it = variables.find(var_name);
-        if (it != variables.end()) return it->second;
-        throw std::runtime_error("Undefined variable: " + var_name);
     }
 
     std::string parse_string(const std::string& text) {
         std::string result = text;
         size_t pos = 0;
-        
         while ((pos = result.find("{{", pos)) != std::string::npos) {
+            size_t end = result.find("}}", pos);
+            if (end == std::string::npos) break;
             
-            size_t end_pos = result.find("}}", pos);
+            std::string var = result.substr(pos+2, end-pos-2);
+            var.erase(0, var.find_first_not_of(" \t"));
+            var.erase(var.find_last_not_of(" \t") + 1);
             
-            if (end_pos == std::string::npos) break;
-            std::string var_name = result.substr(
-                pos + 2,
-                end_pos - pos - 2
-                );
-                
-            var_name.erase(0, var_name.find_first_not_of(" \t"));
-            var_name.erase(var_name.find_last_not_of(" \t") + 1);
-            
-            if (variables.find(var_name) != variables.end()) {
-            	result.replace(
-            	    pos,
-            	    end_pos - pos + 2,
-            	    variables[var_name]
-            	);
+            auto it = variables.find(var);
+            if (it != variables.end()) {
+                result.replace(pos, end-pos+2, it->second);
+                pos += it->second.length();
             } else {
-            	result.erase(pos, end_pos - pos + 2);
+                result.erase(pos, end-pos+2);
             }
         }
         return result;
+    }
+
+    bool evaluate_condition(const std::string& expr) {
+        std::string e = parse_string(expr);
+        size_t op_pos;
+        
+        if ((op_pos = e.find("==")) != std::string::npos) {
+            std::string lhs = e.substr(0, op_pos);
+            std::string rhs = e.substr(op_pos+2);
+            return variables[lhs] == rhs;
+        }
+        if ((op_pos = e.find("!=")) != std::string::npos) {
+            std::string lhs = e.substr(0, op_pos);
+            std::string rhs = e.substr(op_pos+2);
+            return variables[lhs] != rhs;
+        }
+        return variables[e] == "true";
     }
 
 public:
@@ -93,7 +98,35 @@ public:
     }
 
 private:
+    size_t find_matching_end(const std::string& start_cmd, const std::string& end_cmd) {
+        int depth = 1;
+        for (size_t i = current_line + 1; i < script_lines.size(); ++i) {
+            std::istringstream iss(script_lines[i]);
+            std::string cmd;
+            if (iss >> cmd) {
+                if (cmd == start_cmd) depth++;
+                else if (cmd == end_cmd) depth--;
+                
+                if (depth == 0) return i;
+            }
+        }
+        throw std::runtime_error("Unclosed " + start_cmd);
+    }
+
     void execute(const std::string& command) {
+        if (break_requested) return;
+        
+        if (skip_mode) {
+            if (command.find("endif") != std::string::npos) {
+                skip_depth--;
+                if (skip_depth == 0) skip_mode = false;
+            }
+            else if (command.find("if") != std::string::npos) {
+                skip_depth++;
+            }
+            return;
+        }
+
         std::istringstream iss(command);
         std::string cmd;
         iss >> cmd;
@@ -101,21 +134,19 @@ private:
         if (cmd == "set") {
             std::string var, value;
             iss >> var;
-            std::getline(iss >> std::ws, value);
+            getline(iss >> std::ws, value);
             variables[var] = parse_string(value);
         }
         else if (cmd == "print") {
             std::string text;
             getline(iss >> std::ws, text);
-            
-            text = parse_string(text);
-            std::cout << text << std::endl;
+            std::cout << parse_string(text) << std::endl;
         }
         else if (cmd == "input") {
             std::string var;
             iss >> var;
             std::string input;
-            std::getline(std::cin >> std::ws, input);
+            std::getline(std::cin, input);
             variables[var] = input;
         }
         else if (cmd == "call") {
@@ -127,28 +158,43 @@ private:
             }
         }
         else if (cmd == "loop") {
-            loop_ends.push(skip_to_end(current_line, "endloop"));
+            loop_stack.push(current_line);
         }
         else if (cmd == "endloop") {
-            if (!loop_ends.empty()) {
-                current_line = loop_ends.top() - 1;
-                loop_ends.pop();
+            if (!loop_stack.empty()) {
+                current_line = loop_stack.top() - 1;
+                loop_stack.pop();
+            }
+        }
+        else if (cmd == "if") {
+            std::string condition;
+            getline(iss >> std::ws, condition);
+            bool result = evaluate_condition(condition);
+            
+            if (!result) {
+                skip_mode = true;
+                skip_depth = 1;
+                if_stack.push(find_matching_end("if", "endif"));
+            }
+            else {
+                if_stack.push(find_matching_end("if", "endif"));
+            }
+        }
+        else if (cmd == "endif") {
+            if (!if_stack.empty()) {
+                current_line = if_stack.top();
+                if_stack.pop();
+            }
+        }
+        else if (cmd == "break") {
+            if (!loop_stack.empty()) {
+                size_t loop_end = find_matching_end("loop", "endloop");
+                current_line = loop_end;
+                break_requested = true;
             }
         }
         else if (cmd == "exit") {
             exit_requested = true;
         }
-        // Добавьте обработку других команд по аналогии
-    }
-
-    size_t skip_to_end(size_t start, const std::string& end_cmd) {
-        for (size_t i = start + 1; i < script_lines.size(); ++i) {
-            std::istringstream iss(script_lines[i]);
-            std::string cmd;
-            if (iss >> cmd && cmd == end_cmd) {
-                return i;
-            }
-        }
-        throw std::runtime_error("Missing " + end_cmd);
     }
 };
